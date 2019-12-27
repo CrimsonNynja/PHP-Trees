7f2cae870268a825c121023d1f8d9dce43ef39ba
Hudson Cassidy <hudoc96@hotmail.com>
2019-12-28 06:47:03 +1100

    B-Tree init added function skeletons

---
+++ src/PhpTrees/BTree.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace PhpTrees;
+
+class BTree
+{
+    /* the root of the tree */
+    private $root = null;
+
+    public function __construct($rootValue = null)
+    {
+
+    }
+
+    public function insert($value) : void
+    {
+
+    }
+
+    public function getRoot()
+    {
+
+    }
+
+    public function find()
+    {
+
+    }
+
+    public function hasValue()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
---
+++ tests/BTreeTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PhpTrees\BTree;
+
+final class BTreeTest extends TestCase
+{
+
+}
