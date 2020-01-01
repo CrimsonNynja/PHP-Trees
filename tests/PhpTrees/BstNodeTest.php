af81e94a7428c03b767c4d26a7025b4bb9abf08f
Hudson Cassidy <hudoc96@hotmail.com>
2020-01-01 19:59:15 +1100

    BST node is now compatible with custom comparitors Renamed Node to BstNode to better reflect its use updated readme

--- tests/PhpTrees/BstNodeTest.php
+++ tests/PhpTrees/BstNodeTest.php
@@ -1,29 +1,29 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use PhpTrees\Node;
+use PhpTrees\BstNode;
 
-final class NodeTest extends TestCase
+final class BstNodeTest extends TestCase
 {
     public function testConstruction()
     {
-        $n = new Node(5);
+        $n = new BstNode(5);
         $this->assertSame($n->getValue(), 5);
         $this->assertNull($n->getLeftChild());
         $this->assertNull($n->getRightChild());
         $this->assertNull($n->getParent());
         $id = $n->getId();
 
-        $n2 = new Node(1);
-        $n2 = new Node(1);
-        $n2 = new Node(1);
+        $n2 = new BstNode(1);
+        $n2 = new BstNode(1);
+        $n2 = new BstNode(1);
 
         $this->assertSame($n2->getId(), $id + 3);
     }
 
     public function testAddChild()
     {
-        $n = new Node(5);
+        $n = new BstNode(5);
         $n->addChild(3);
         $n->addChild(7);
         $this->assertNull($n->getParent());
@@ -35,7 +35,7 @@ final class NodeTest extends TestCase
 
     public function testRemoveChild()
     {
-        $n = new Node(5);
+        $n = new BstNode(5);
         $id = $n->getId();
 
         $n->removeChild(6);
@@ -55,7 +55,7 @@ final class NodeTest extends TestCase
 
     public function testIsLeaf()
     {
-        $n = new Node(5);
+        $n = new BstNode(5);
         $this->assertTrue($n->isLeaf());
 
         $n->addChild(3);
@@ -67,15 +67,44 @@ final class NodeTest extends TestCase
 
     public function testHasChild()
     {
-        $n = new Node(5);
-        $this->assertFalse($n->hasChild(new Node(6)));
+        $n = new BstNode(5);
+        $this->assertFalse($n->hasChild(new BstNode(6)));
 
         $n->addChild(8);
-        $this->assertFalse($n->hasChild(new Node(9)));
+        $this->assertFalse($n->hasChild(new BstNode(9)));
         $this->assertTrue(($n->hasChild($n->getRightChild())));
 
         $n->addChild(2);
-        $this->assertFalse($n->hasChild(new Node(2)));
+        $this->assertFalse($n->hasChild(new BstNode(2)));
         $this->assertTrue(($n->hasChild($n->getLeftChild())));
+    }
+
+    public function testAddComparitor()
+    {
+        $n = new BstNode("12345");
+        $n->setComparitor(function($val, $val2) {
+            return (strlen($val) <= strlen($val2));
+        });
+        $n->addChild("1234");
+        $n->addChild("12345678");
+        $n->addChild("12");
+        $n->addChild("1234");
+
+        $this->assertSame($n->getValue(), "12345");
+        $this->assertSame($n->getLeftChild()->getValue(), "1234");
+        $this->assertSame($n->getRightChild()->getValue(), "12345678");
+        $this->assertSame($n->getLeftChild()->getLeftChild()->getValue(), "12");
+        $this->assertSame($n->getLeftChild()->getRightChild()->getValue(), "1234");
+    }
+
+    public function testHasComparitor()
+    {
+        $n = new BstNode("12345");
+        $this->assertFalse($n->hasComparitor());
+
+        $n->setComparitor(function($v, $v2){
+            return true;
+        });
+        $this->assertTrue($n->hasComparitor());
     }
 }
